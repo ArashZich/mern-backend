55b0b24bedfed2e5d9b89659cb5f36ca1c7c85a7
ArashZich <arashzich1992@gmail.com>
2022-03-31 17:23:53 +0430

    start connect mongo and back

--- controllers/posts-controllers.js
+++ controllers/posts-controllers.js
@@ -1,6 +1,7 @@
 const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
+const Post = require("../models/post");
 
 let posts = [
   {
@@ -11,15 +12,35 @@ let posts = [
   },
 ];
 
-const getPostById = (req, res, next) => {
+// const getPostById = (req, res, next) => {
+//   const postId = req.params.pid;
+//   const post = posts.find((p) => {
+//     return p.id === postId;
+//   });
+//   if (!post) {
+//     return next(new HttpError("Not Found!", 404));
+//   }
+//   res.json({ post: post });
+// };
+
+const getPostById = async (req, res, next) => {
   const postId = req.params.pid;
-  const post = posts.find((p) => {
-    return p.id === postId;
-  });
-  if (!post) {
-    return next(new HttpError("Not Found!", 404));
+  let post;
+
+  try {
+    post = await Post.findById(postId);
+  } catch (err) {
+    const error = new HttpError("Could not find a post", 500);
+    return next(error);
   }
-  res.json({ post: post });
+
+  if (!post) {
+    const error = new HttpError("Could not find a post", 500);
+    return next(error);
+  }
+
+  res.json({ post: post.toObject({ getters: true }) });
+  //getters: true => {_id => id}
 };
 
 const getPostByUserId = (req, res, next) => {
@@ -32,20 +53,29 @@ const getPostByUserId = (req, res, next) => {
   res.json({ post: post });
 };
 
-const createPost = (req, res, next) => {
+const createPost = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError("Invalid Inputs", 422);
     // 422 => invalid value
   }
   const { title, description, creator } = req.body;
-  const createdPost = {
-    id: uuidv4(),
+
+  const createdPost = new Post({
     title: title,
     description: description,
+    image: "url",
     creator: creator,
-  };
-  posts.push(createdPost);
+    // id: uuidv4(),
+  });
+
+  try {
+    await createdPost.save();
+  } catch (err) {
+    const error = new HttpError("Creating Post Failed!", 500);
+    return next(error);
+  }
+
   res.status(201).json({ post: createdPost });
   //201 => success and change value
 };
