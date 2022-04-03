b91dc6ac7f10a512ff4ad6b63334fea98a684f08
ArashZich <arashzich1992@gmail.com>
2022-04-03 10:51:45 +0430

    The End

---
+++ middleware/check-auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const HttpError = require("../models/http-error");
+
+module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+
+  try {
+    const token = req.headers.authorization.split(" ")[1]; //'Bearer TOKEN
+
+    if (!token) {
+      throw new Error("Auth failed!");
+    }
+
+    const decodedToken = jwt.verify(token, "secret_key");
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (err) {
+    const error = new HttpError("Auth failed!", 401);
+    return next(error);
+  }
+};
--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -2,12 +2,17 @@ const express = require("express");
 const { check } = require("express-validator");
 const postControllers = require("../controllers/posts-controllers");
 const fileUpload = require("../middleware/file-upload");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
 router.get("/:pid", postControllers.getPostById);
 
 router.get("/user/:uid", postControllers.getPostByUserId);
+
+//! route protection ==> before token necessary routes
+router.use(checkAuth);
+//!
 
 router.post(
   "/",
